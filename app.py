d2838f8ca7b2ac830a07c3d25cccbbbe7540b123
swiercm2 <marcin.swierczyna@contractors.roche.com>
2019-02-22 09:37:20 +0100

    added train network endpoint

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from tornado import web, ioloop
 
-from handlers import NetworkTrainHandler, NetworkPredictHandler
+from handlers import NetworkTrainHandler
 
 
-def make_app(**config):
+def make_app():
+    settings = {
+        'api': 'http://localhost/api',
+        'size_input_image': (32, 32)
+    }
     return web.Application([
         (r"/train/", NetworkTrainHandler),
-        (r"/predict/", NetworkPredictHandler)
-    ], **config)
+    ], **settings)
 
 
 if __name__ == '__main__':
-    app = make_app(debug=True, api='http://localhost/api')
+    app = make_app()
     app.listen(8080)
     ioloop.IOLoop.current().start()
--- handlers.py
+++ handlers.py
@@ -1,38 +1,53 @@
 import json
 
-from tornado import web, httpclient
+from tornado import httpclient, ioloop, web
 
 from nn.VGGNet import VGGNet
 
 
 class NetworkTrainHandler(web.RequestHandler):
+    def __init__(self, *args, **kwargs):
+        super(NetworkTrainHandler, self).__init__(*args, **kwargs)
+        self.client = httpclient.AsyncHTTPClient()
+
     async def post(self):
         network_id = self.get_argument('network_id')
-        network_data = await self.get_network_data(network_id)
         operation_data = await self.add_operation(network_id)
-        network = self.get_network(len(network_data['collections']))
+        ioloop.IOLoop.current().spawn_callback(self.train_network, network_id)
         self.write(json.dumps(operation_data))
 
+    async def train_network(self, network_id):
+        network_data = await self.get_network_data(network_id)
+        training_data = await self.get_training_data(network_data['collections'])
+        network = VGGNet.get_network(network_data['collections'], self.settings['size_input_image'])
+        await network.train(training_data, 20, 15)
+
     async def get_network_data(self, network_id):
-        client = httpclient.AsyncHTTPClient()
-        response = await client.fetch(
-            'http://localhost/api/networks/%s/?embedded={"collections":1,"collections.images":1}' % network_id)
+        url = f'{self.settings["api"]}/networks/{network_id}/?embedded={{"collections":1,"collections.images":1}}'
+        response = await self.client.fetch(url)
         return json.loads(response.body)
+
+    async def get_training_data(self, collections):
+        training_data = []
+        for collection in collections:
+            for image_data in collection['images']:
+                image = await self.get_image(image_data['image'])
+                training_data.append((image, collection))
+        return training_data
+
+    async def get_image(self, image_url):
+        url = f'{self.settings["api"]}{image_url}'
+        response = await self.client.fetch(url)
+        return response.body
 
     async def add_operation(self, network_id, step=0.01):
+        url = f'{self.settings["api"]}/operations'
         headers = {'Content-Type': 'application/json'}
-        client = httpclient.AsyncHTTPClient()
-        body = json.dumps({'name': 'train %s' % network_id, 'network': network_id, 'step': step})
-        response = await client.fetch("http://localhost/api/operations", method='POST', body=body, headers=headers)
+        body = json.dumps({'name': f'train{network_id}',
+                           'network': network_id,
+                           'step': step})
+        response = await self.client.fetch(url, method='POST', body=body, headers=headers)
         return json.loads(response.body)
 
-    def get_network(self, number_classes):
-        return VGGNet.get_network(number_classes)
-
-
-class NetworkPredictHandler(web.RequestHandler):
-    def post(self):
-        network_id = self.get_argument('network_id')
-        image_id = self.get_argument('image_id')
-        result = None
-        self.write(result)
+    def data_received(self, chunk):
+        return chunk
