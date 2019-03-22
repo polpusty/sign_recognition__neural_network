3e3467b24b8db264f7ff3fd4f1c6f36b6b7720d3
swiercm2 <marcin.swierczyna@contractors.roche.com>
2019-03-22 06:25:07 +0100

    added uploading network after training

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 import json
+import mimetypes
+import uuid
 
 from tornado import httpclient
 
@@ -35,3 +37,43 @@ class ApiClient:
                            'step': step})
         response = await self.client.fetch(url, method='POST', body=body, headers=headers)
         return json.loads(response.body)
+
+    async def upload_network(self, network_data, network):
+        url = f'{self.api_url}/networks/{network_data["_id"]}/'
+        files = {'trained': (f'{network_data["_id"]}.bin', str(network))}
+        boundary = uuid.uuid4().hex
+        body = self.encode_form_data(files=files, boundary=boundary)
+        headers = {'If-Match': network_data['_etag'],
+                   'Content-Type': f'multipart/form-data; boundary={boundary}'}
+
+        response = await self.client.fetch(url, method='PATCH', body=body, headers=headers, raise_error=False)
+        return response.body
+
+    def encode_form_data(self, files=None, fields=None, boundary=''):
+        if fields is None:
+            fields = {}
+        if files is None:
+            files = {}
+        lines = []
+
+        for key, value in fields.items():
+            lines.append(f'--{boundary}')
+            lines.append(f'Content-Disposition: form-data; name="{key}"')
+            lines.append('')
+            lines.append(value)
+
+        for key, (filename, value) in files.items():
+            lines.append(f'--{boundary}')
+            lines.append(f'Content-Disposition: form-data; name="{key}"; filename="{filename}"')
+            lines.append(f'Content-Type: {self.get_content_type(filename)}')
+            lines.append('')
+            lines.append(value)
+
+        lines.append(f'--{boundary}--')
+        lines.append('')
+
+        return '\r\n'.join(lines)
+
+    @staticmethod
+    def get_content_type(filename):
+        return mimetypes.guess_type(filename)[0] or 'binary'
--- handlers.py
+++ handlers.py
@@ -1,5 +1,7 @@
+import io
 import json
 
+import dill
 from tornado import ioloop, web
 
 from api import ApiClient
@@ -22,7 +24,13 @@ class NetworkTrainHandler(web.RequestHandler):
         training_data = await self.client.get_training_data(network_data['collections'])
         network = LeNet.get_network(list(map(lambda collection: collection['class_code'], network_data['collections'])),
                                     (32, 32))
-        await network.fit(training_data, 150, 15)
+        await network.fit(training_data, 15, 15)
+        await self.upload_trained_network(network, network_data)
+
+    async def upload_trained_network(self, network, network_data):
+        network_bytes = io.BytesIO()
+        dill.dump(network, network_bytes)
+        await self.client.upload_network(network_data, network_bytes.getvalue())
 
     def data_received(self, chunk):
         return chunk
