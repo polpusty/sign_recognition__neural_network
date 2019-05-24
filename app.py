7e6cd86a0b598cf32dee49d26401177f88b5baf4
swiercm2 <marcin.swierczyna@contractors.roche.com>
2019-05-24 13:24:31 +0200

    merged

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from tornado import web, ioloop
 
 from handlers import NetworkTrainHandler, NetworkPredictHandler
@@ -19,25 +18,3 @@ if __name__ == '__main__':
     app = make_app()
     app.listen(80)
     ioloop.IOLoop.current().start()
-=======
-from tornado import web, ioloop
-
-from handlers import NetworkTrainHandler, NetworkPredictHandler
-
-
-def make_app():
-    settings = {
-        'api': 'http://localhost/api',
-        'size_input_image': (32, 32)
-    }
-    return web.Application([
-        (r"/train/", NetworkTrainHandler),
-        (r"/predict/", NetworkPredictHandler),
-    ], **settings)
-
-
-if __name__ == '__main__':
-    app = make_app()
-    app.listen(80)
-    ioloop.IOLoop.current().start()
->>>>>>> be1166b6555c110b5013a51645a58c3c880c2925
